992205ea327c57d4f2efcc017ed4e627e393164f
toli444 <toli44777@gmail.com>
2024-05-18 12:42:00 +0200

    disabling minification

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 const ReactCompilerConfig = {};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // disabling minification, so we can examine the dist folder and see the output of the React Compiler
+    minify: false,
+  },
   plugins: [
     react({
       babel: {
